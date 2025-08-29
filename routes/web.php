af2d366d1034f52c62ea324b4dba3624a86285fd
Fabio Costa <costantinofabio84@gmail.com>
2025-08-29 03:11:28 +0200

    Route di prova

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,9 @@ Route::view('/', 'welcome');
 Route::post('/cities', [CityController::class, 'store'])->name('cities.store');
 Route::get('/cities/{city}/stats', [CityController::class, 'stats'])->name('cities.stats');
 
+Route::get('/test-stats', function () {
+    return view('city.stats', ['city' => (object) [
+        'name' => 'Roma',
+        'country' => 'IT'
+    ]]);
+});
