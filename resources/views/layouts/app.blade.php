bf16c40102f8135da9834cc7757fac95be032363
Fabio Costa <costantinofabio84@gmail.com>
2025-08-31 00:58:09 +0200

    Aggiunto cdn bootstrap in app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,8 +4,13 @@
   <meta charset="utf-8">
   <title>{{ config('app.name', 'Weather Dashboard') }}</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  @vite(['resources/js/app.js', 'resources/css/app.css'])
+  {{-- @vite(['resources/js/app.js', 'resources/css/app.css']) --}}
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
 </head>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
 <body>
   <div class="container py-4">
     @yield('content')
